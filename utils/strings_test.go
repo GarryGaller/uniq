bd895606de8bac2250305c7a85444d5cdaa9062a
GarryGaller <ggaller@mail.ru>
2021-09-17 21:53:29 +0300

    Added benchmarks

--- utils/strings_test.go
+++ utils/strings_test.go
@@ -3,8 +3,13 @@ package utils
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
+    "math/rand"
 	"os"
 	"strings"
+	"time"
+    "testing"
+    
 	"uniq/cli"
 )
 
@@ -146,3 +151,76 @@ func ExampleCounterLinesByPrefixIgnoreCase() {
 	// Output:
 	// 2 aa
 }
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func GenerateRandomStrings(count int) (outline string) {
+	var builder strings.Builder
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < count; i++ {
+		builder.WriteString(randSeq(10) + "\n")
+	}
+	outline = builder.String()
+	return
+}
+
+func BenchmarkUnique10000(b *testing.B) {
+
+	testFile := GenerateRandomStrings(10000)
+	var reader = strings.NewReader(testFile)
+	var writer = ioutil.Discard
+	cmd.Mapper = func(s string) string { return s }
+	cmd.Cutter = func(s string) string { return s }
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Unique(reader, writer, cmd)
+	}
+}
+
+func BenchmarkDuplicates10000(b *testing.B) {
+
+	testFile := GenerateRandomStrings(10000)
+	var reader = strings.NewReader(testFile)
+	var writer = ioutil.Discard
+	cmd.Mapper = func(s string) string { return s }
+	cmd.Cutter = func(s string) string { return s }
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Duplicates(reader, writer, cmd)
+	}
+}
+
+func BenchmarkDeduplicate10000(b *testing.B) {
+
+	testFile := GenerateRandomStrings(10000)
+	var reader = strings.NewReader(testFile)
+	var writer = ioutil.Discard
+	cmd.Mapper = func(s string) string { return s }
+	cmd.Cutter = func(s string) string { return s }
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Deduplicate(reader, writer, cmd)
+	}
+}  
+
+
+// go test
+// benchmarks
+//go test -bench=. -benchtime=10x -benchmem 
+/*
+BenchmarkUnique10000-4                10            400030 ns/op           36148 B/op       2001 allocs/op
+BenchmarkDuplicates10000-4            10            100000 ns/op           20096 B/op       1001 allocs/op
+BenchmarkDeduplicate10000-4           10            400020 ns/op           36170 B/op       2001 allocs/op
+*/
